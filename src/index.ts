401f5bb71b2fcea28664c66fe7b3e73118af0391
Sworup Poudel <sworup5137@gmail.com>
2025-05-09 14:32:23 -0500

    add cors

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import connectDB from './config/db';
 import dotenv from 'dotenv';
 import employeeRoutes from './routes/employeeRoutes';
+import cors from 'cors';
 
 dotenv.config();
 
@@ -12,6 +13,7 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 // Middleware
 app.use(express.json());
+app.use(cors());
 
 app.use('/api/employees', employeeRoutes);
 
